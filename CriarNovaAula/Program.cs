51fc32b436f24db6f519b235eed660a850e5890f
devmkpro <maikeofc+1@gmail.com>
2024-02-20 14:52:22 -0300

    Atualizações de código

---
+++ CriarNovaAula/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace CriarNovaAula
+{
+  class Program
+  {
+    private string _diretorio;
+
+    public Program(string diretorio)
+    {
+      _diretorio = diretorio;
+    }
+
+    static void Main(string[] args)
+    {
+      Program programa = new Program("classroom");
+      programa.CriarPastaEProjeto();
+    }
+
+    void CriarPastaEProjeto()
+    {
+      string nomeAula = ObterNomeAula();
+      string path = Path.Combine(Directory.GetCurrentDirectory(), "..", _diretorio, nomeAula);
+
+      if (!Directory.Exists(path))
+      {
+        Directory.CreateDirectory(path);
+        Console.WriteLine($"Pasta '{nomeAula}' criada com sucesso!");
+      }
+      else
+      {
+        Console.WriteLine($"A pasta '{nomeAula}' já existe.");
+      }
+
+      // Navega para o diretório da aula
+      Directory.SetCurrentDirectory(path);
+
+      string nomeProjeto = ObterNomeProjeto();
+
+      // Cria o projeto C#
+      string comando = $"dotnet new console -n {nomeProjeto}";
+      ExecutarComando(comando);
+
+      Console.WriteLine($"Projeto '{nomeProjeto}' criado com sucesso em '{nomeAula}'.");
+    }
+
+    string ObterNomeAula()
+    {
+      Console.WriteLine("Digite o nome da aula: ");
+      string? nomeAula = Console.ReadLine();
+
+      while (string.IsNullOrEmpty(nomeAula))
+      {
+        Console.WriteLine("Nome da aula inválido.");
+        Console.WriteLine("Digite o nome da aula: ");
+        nomeAula = Console.ReadLine();
+      }
+
+      return nomeAula;
+    }
+
+    string ObterNomeProjeto()
+    {
+      Console.Write("Digite o nome do projeto C#: ");
+      string? nomeProjeto = Console.ReadLine();
+
+      while (string.IsNullOrEmpty(nomeProjeto))
+      {
+        Console.WriteLine("Nome do projeto inválido.");
+        Console.Write("Digite o nome do projeto C#: ");
+        nomeProjeto = Console.ReadLine();
+      }
+
+      return nomeProjeto;
+    }
+
+    void ExecutarComando(string comando)
+    {
+      try
+      {
+        System.Diagnostics.Process.Start("cmd.exe", $"/c {comando}").WaitForExit();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Erro ao executar o comando: {ex.Message}");
+      }
+    }
+  }
+}
---
+++ ofMe/JsonPlaceHolderClient/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Http.Json;
+
+namespace JsonPlaceholderClient
+{
+  class Program
+  {
+    
+    private readonly string _baseUrl;
+
+    public Program(string baseUrl)
+    {
+      _baseUrl = baseUrl;
+    }
+ 
+    
+    static async Task Main(string[] args)
+    {
+      var program = new Program("https://jsonplaceholder.typicode.com");
+      
+      await program.GetPostsAsync();
+
+      Console.ReadKey();
+    }
+
+
+    public async Task GetPostsAsync()
+    {
+      using var httpClient = new HttpClient();
+
+      try 
+      {
+        HttpResponseMessage response = await httpClient.GetAsync($"{_baseUrl}/posts");
+
+        response.EnsureSuccessStatusCode(); // verificando se a resposta foi bem sucedida
+
+        List<Post>? posts = await response.Content.ReadFromJsonAsync<List<Post>>();
+
+        if (posts != null && posts.Count > 0)
+        {
+          foreach (var post in posts)
+          {
+            Console.WriteLine($"Id: {post.Id}");
+            Console.WriteLine($"Title: {post.Title}");
+            Console.WriteLine($"Body: {post.Body}");
+            Console.WriteLine();
+          }
+        }
+        else 
+        {
+          Console.WriteLine("No posts were found");
+        }
+
+      }
+        catch (HttpRequestException e)
+        {
+          Console.WriteLine($"Request exception: {e.Message}");
+        }
+    }
+
+  }
+
+  public class Post
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+    public Post(int id, string title, string body)
+    {
+      Id = id;
+      Title = title;
+      Body = body;
+    }
+  }
+}
